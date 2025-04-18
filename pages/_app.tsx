cdfd95b8d5a2f715469cfa973b9dcde3327898ea
John Cafferty <johncaffertyx@gmail.com>
2025-04-18 19:47:44 +0100

    Enable WhatsApp button across all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import WhatsAppButton from '../components/WhatsAppButton';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <WhatsAppButton />
+    </>
+  );
 }
 
 export default MyApp;
