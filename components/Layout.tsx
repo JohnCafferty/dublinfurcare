28ed31d801046003b63562b544fc8cc63c42e8c5
John Cafferty <johncaffertyx@gmail.com>
2025-05-26 10:17:05 +0100

    Add favicon and structured logo using android-chrome-192x192.png

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 // components/Layout.tsx
 
 import { ReactNode } from "react";
+import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -11,6 +12,33 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Head>
+        {/* Favicon */}
+        <link
+          rel="icon"
+          type="image/png"
+          href="/images/android-chrome-192x192.png"
+          sizes="192x192"
+        />
+        <link
+          rel="shortcut icon"
+          href="/images/android-chrome-192x192.png"
+        />
+
+        {/* Logo for Google Search */}
+        <script type="application/ld+json">
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "Dublin Pet Grooming",
+            "url": "https://www.dublinpetgrooming.ie",
+            "logo": "https://www.dublinpetgrooming.ie/images/android-chrome-192x192.png"
+          }
+          `}
+        </script>
+      </Head>
+
       <Header />
       <main className="font-inter bg-[#fdfcfb] text-gray-900 scroll-smooth">
         {children}
